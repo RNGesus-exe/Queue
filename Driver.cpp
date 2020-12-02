a871f5af0ef48983d12598dc7296e816f344a7ef
rngesus <behzadshaheryar@gmail.com>
2020-12-02 22:24:53 +0500

    Added Circular Queue

--- Driver.cpp
+++ Driver.cpp
@@ -41,7 +41,7 @@ public:
     int peekRear(){
         return arr[front];
     }
-    void display(){
+    void display() const{
         for(int i=0;i<=front;i++){
             std::cout<<arr[i]<<" ";
         }
@@ -55,19 +55,86 @@ public:
     }
 };
 
-
+template <class T>
+class CircularQueue{
+    T* arr;
+    int front;
+    int rear;
+    int size;
+public:
+    CircularQueue():arr(nullptr),front(0),rear(0),size(0){}
+    CircularQueue(int size):size(size),front(0),rear(0),arr(new T[size]){}
+    bool isEmpty(){
+        return front == rear;
+    }
+    bool isFull(){
+        return (front == ((rear+1) % (size)));
+    }
+    void enqueue(T data){
+        if(!isFull()){
+            arr[++rear%size] = data;
+        }
+        else{
+            std::cerr<<"There is no space to enqueue!!!"<<std::endl;
+        }
+    }
+    T* dequeue(){
+        T* tmp = nullptr;
+        if(!isEmpty()){
+            tmp = new T;
+            *tmp = arr[++front % size];
+        }
+        return tmp;
+    }
+    void display() const{
+        for(int i=front+1;i<=rear;i++){
+            std::cout<<arr[i%size]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+    ~CircularQueue(){
+        if(arr!=nullptr){
+            delete[] arr;
+            front = rear = size = 0;
+        }
+    }
+    void dequeue_print(){
+        int *tmp = this->dequeue();
+        std::cout<<*tmp<<std::endl;
+        delete tmp;
+    }
+};
 
 int main(){
+    int size = 5;
+    CircularQueue<int> cq(size+1);
+    cq.enqueue(10);
+    cq.enqueue(20);
+    cq.enqueue(69);
+    cq.enqueue(79);
+    cq.enqueue(44);
+    //========= F 10 20 69 79 44(R) =====
+    cq.enqueue(1);
+    cq.enqueue(1);
+    cq.enqueue(1);
+    //========= F 10 20 69 79 44(R) =====
+    cq.dequeue();
+    cq.dequeue();
+    cq.dequeue();
+    //========= 0 0 0 F 79 44(R)=====
+    cq.enqueue(10);
+    cq.enqueue(10);
+    cq.enqueue(10);
+    //========= 10 10 10(R) F 79 44====
+    cq.dequeue();
+    cq.dequeue();
+    //========= 10 10 10(R) 0 0 F======
+    cq.enqueue(5);
+    cq.enqueue(10);
+    //========= 10 10 10 5 10(R) F====
+    cq.dequeue();
+    cq.dequeue();
+    //========= 0 F 10 5 10(R) 0====
 
-    Queue q(10);
-    q.enqueue(10);
-    q.enqueue(30);
-    q.enqueue(15);
-    q.enqueue(12);
-    q.display();
-    q.dequeue();
-    q.display();
-    std::cout<<q.peekFront()<<
-    q.peekRear()<<std::endl;
     return 0;
 }
